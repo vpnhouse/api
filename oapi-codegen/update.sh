6850e682abb9c592829aa705aa3e5d14500a724a
Dmitrii Efimov <efimov-d-i@yandex.ru>
2023-04-04 06:17:21 +0300

    add userservice, clientservice and projectservice

--- oapi-codegen/update.sh
+++ oapi-codegen/update.sh
@@ -27,6 +27,48 @@ END
 sed -i -e 's/*Error/*externalRef1.Error/g' $target
 sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
 
+### fix external ref bug for the user_service-client
+target=../go/client/user_service/user_service.go
+ln=$(grep -m1 -n "github.com/deepmap/oapi-codegen/pkg/runtime" $target | cut -d: -f1)
+ed $target << END
+${ln}a
+	externalRef1 "github.com/vpnhouse/api/go/server/common"
+.
+w
+q
+END
+
+sed -i -e 's/*Error/*externalRef1.Error/g' $target
+sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
+
+### fix external ref bug for the client_service-client
+target=../go/client/client_service/client_service.go
+ln=$(grep -m1 -n "github.com/deepmap/oapi-codegen/pkg/runtime" $target | cut -d: -f1)
+ed $target << END
+${ln}a
+	externalRef1 "github.com/vpnhouse/api/go/server/common"
+.
+w
+q
+END
+
+sed -i -e 's/*Error/*externalRef1.Error/g' $target
+sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
+
+### fix external ref bug for the project_service-client
+target=../go/client/project_service/project_service.go
+ln=$(grep -m1 -n "github.com/deepmap/oapi-codegen/pkg/runtime" $target | cut -d: -f1)
+ed $target << END
+${ln}a
+	externalRef1 "github.com/vpnhouse/api/go/server/common"
+.
+w
+q
+END
+
+sed -i -e 's/*Error/*externalRef1.Error/g' $target
+sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
+
 ### fix external ref bug in the tunnel_mgmt-client
 target=../go/client/tunnel_mgmt/tunnel.go
 ln=$(grep -m1 -n "strings" $target | cut -d: -f1)
