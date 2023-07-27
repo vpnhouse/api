468b34946ccd14dd9ed2fa67acef4aeaab91fa43
Dmitrii Efimov <efimov-d-i@yandex.ru>
2023-07-27 17:03:50 +0300

    add license service

--- oapi-codegen/update.sh
+++ oapi-codegen/update.sh
@@ -69,6 +69,20 @@ END
 sed -i -e 's/*Error/*externalRef1.Error/g' $target
 sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
 
+### fix external ref bug for the license_service-client
+target=../go/client/license_service/license_service.go
+ln=$(grep -m1 -n "github.com/deepmap/oapi-codegen/pkg/runtime" $target | cut -d: -f1)
+ed $target << END
+${ln}a
+	externalRef1 "github.com/vpnhouse/api/go/server/common"
+.
+w
+q
+END
+
+sed -i -e 's/*Error/*externalRef1.Error/g' $target
+sed -i -e 's/var dest Error/var dest externalRef1.Error/g' $target
+
 ### fix external ref bug in the tunnel_mgmt-client
 target=../go/client/tunnel_mgmt/tunnel.go
 ln=$(grep -m1 -n "strings" $target | cut -d: -f1)
