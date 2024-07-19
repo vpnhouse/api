8ae0f8027e71efe25f01313f3f4b7f67c77af96d
DmitriiPyshinskii <149574053+DmitriiPyshinskii@users.noreply.github.com>
2024-07-19 20:59:26 +0700

    fix signedPayload format && add docker-image generator (#83)
    * fix signedPayload format && add docker-image generator
    
    * update licenseservice api
    
    ---------
    
    Co-authored-by: Dmitrii Pyshinskii <PyshinskiyDI@sibedge.com>

--- oapi-codegen/update.sh
+++ oapi-codegen/update.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # note: the func exists because of this issue:
 # https://github.com/deepmap/oapi-codegen/issues/377
 fix_external_ref_bug() {
-    target=$1
+    target="$1"
     import_line="import ("
     ln=$(grep -m1 -n "$import_line" "$target" | cut -d: -f1)
     ed "$target" << END
@@ -18,24 +18,25 @@ END
     sed -i -e 's/var dest Error/var dest externalRef1.Error/g' "$target"
 }
 
+# Loop through all .conf files in the current directory
 for conf_name in *.conf; do
     schema_name="../schemas/${conf_name%%.*}.yaml"
     echo "$schema_name..."
     oapi-codegen -config "$conf_name" "$schema_name"
 done
 
-targets=(
-    "../go/client/authorizer/authorizer.go"
-    "../go/client/client_service/client_service.go"
-    "../go/client/dashboard/dashboard.go"
-    "../go/client/discovery/discovery.go"
-    "../go/client/federation/federation.go"
-    "../go/client/license_service/license_service.go"
-    "../go/client/project_service/project_service.go"
-    "../go/client/user_service/user_service.go"
+# Define the array of targets using set command
+set -- "../go/client/authorizer/authorizer.go" \
+    "../go/client/client_service/client_service.go" \
+    "../go/client/dashboard/dashboard.go" \
+    "../go/client/discovery/discovery.go" \
+    "../go/client/federation/federation.go" \
+    "../go/client/license_service/license_service.go" \
+    "../go/client/project_service/project_service.go" \
+    "../go/client/user_service/user_service.go" \
     "../go/client/tunnel_mgmt/tunnel.go"
-)
 
-for target in "${targets[@]}"; do
+# Loop through the array of targets
+for target; do
     fix_external_ref_bug "$target"
 done
